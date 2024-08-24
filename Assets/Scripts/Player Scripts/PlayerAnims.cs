b8fc11f2a46128e6761198da38ced261395e2e1b
Dylan Torres <glaciershield@gmail.com>
2024-08-23 21:02:43 -0400

    Animation and Movement In Progress
    The player's animations and movements are in progress.

---
+++ Assets/Scripts/Player Scripts/PlayerAnims.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(PlayerController))]
+
+public class PlayerAnims : MonoBehaviour
+{
+    [SerializeField] private PlayerController pC;
+    [SerializeField] Animator m_Animator;
+    [SerializeField] bool isGrounded, isMoving, isOnWall, isParrying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Inputs used to switch between idle animation and moving animation.
+        bool hasHorizontalInput = !Mathf.Approximately(pC.horizontalInput, 0f);
+        bool movementCheck = hasHorizontalInput;
+    }
+}
--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [Header("Player Movement")]
+    [SerializeField] private float movementSpeed; // Player's movement speed.
+    [SerializeField] private float horizontalInput; // Player's horizontal input.
+    [SerializeField] private bool groundCheck; // Checks if the player is grounded.
+    [SerializeField] float yGizmoDisplacement; // Edits the y-axis of the player's ground checker gizmo.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerInput(); // Inputs are counted by frame.
+    }
+
+    // FixedUpdate is called once per physics update
+    void FixedUpdate()
+    {
+        PlayerMovement(); // Movement is controlled by physics, so it goes in FixedUpdate.
+    }
+
+    // PlayerInput detects when players press inputs
+    void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal"); // Sets the player's horizontal move input.
+    }
+
+    // PlayerMovement detects when players are moving
+    void PlayerMovement()
+    {
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * Time.deltaTime * movementSpeed);
+    }
+
+    // Gizmos will be drawn to test the ground checker
+    private void OnDrawGizmos()
+    {        
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y - yGizmoDisplacement), new Vector2(1f, 0.2f));
     }
 }

e1419c926c4b671af29234b64d4c61db2350f10f
Dylan Torres <glaciershield@gmail.com>
2024-08-25 19:05:25 -0400

    Missiles Movement Operational
    Missiles now fly to the left. Next, I will be working on UI and SFX implementation (Time Spent: 2 hours and 5 minutes).

--- Assets/Scripts/Missile Scripts/Missile.cs
+++ Assets/Scripts/Missile Scripts/Missile.cs
@@ -6,12 +6,23 @@ public class Missile : MonoBehaviour
 {
     [SerializeField] Collider2D hitbox;
     [SerializeField] Animator m_Animator;
-    
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float xVel; 
+
+    private enum MissileState 
+    {
+        Intact,
+        Burst
+    }
+    [SerializeField] private MissileState stateOfMissile;
+
     // Start is called before the first frame update
     void Start()
     {
         hitbox = gameObject.GetComponent<Collider2D>();
         m_Animator = gameObject.GetComponentInChildren<Animator>();
+        rb = gameObject.GetComponentInChildren<Rigidbody2D>();
+        stateOfMissile = MissileState.Intact;
     }
 
     // Update is called once per frame
@@ -20,10 +31,26 @@ public class Missile : MonoBehaviour
         
     }
 
+    // FixedUpdate is called once per physics update
+    private void FixedUpdate()
+    {
+        if (stateOfMissile != MissileState.Burst)
+        {
+            rb.velocity = new Vector2(xVel, 0f);
+        }
+        else 
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         m_Animator.SetBool("hitsSurface", true); // When a missile hits another collider, play the burst animation.
+        stateOfMissile = MissileState.Burst;
         hitbox.enabled = false;
     }
+
+
 
 }

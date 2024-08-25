edbf0fc95467bb0a409324526f93164fd04e9185
Dylan Torres <glaciershield@gmail.com>
2024-08-25 18:35:17 -0400

    Missile Animations Operational
    The missile now animates and collides as intended. Next, I will program the logic that makes it travel to the player (Time Spent: 1 hour and 35 minutes).

--- Assets/Scripts/Missile Scripts/Missile.cs
+++ Assets/Scripts/Missile Scripts/Missile.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    [SerializeField] Collider2D hitbox;
+    [SerializeField] Animator m_Animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        hitbox = gameObject.GetComponent<Collider2D>();
+        m_Animator = gameObject.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -15,4 +19,11 @@ public class Missile : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_Animator.SetBool("hitsSurface", true); // When a missile hits another collider, play the burst animation.
+        hitbox.enabled = false;
+    }
+
 }

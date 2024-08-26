428e04a70e2b5224ed4bfa15e62bdf2743ca0cc0
Dylan Torres <glaciershield@gmail.com>
2024-08-26 02:12:22 -0400

    O and P Key Toggles Functioning
    Pressing the O and P keys display the checklist and designation, respectively. Next, I will program the UI input history and Character State Indicator (Time Spent: 3 hours and 30 minutes).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject oKeyTextToggle, pKeyTextToggle;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        oKeyTextToggle.SetActive(false);
+        pKeyTextToggle.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OAndPKeyToggle();
     }
+
+    void OAndPKeyToggle() 
+    {
+        if (Input.GetKeyDown(KeyCode.O) && !oKeyTextToggle.activeSelf)
+        {
+            oKeyTextToggle.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.O) && oKeyTextToggle.activeSelf)
+        {
+            oKeyTextToggle.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && !pKeyTextToggle.activeSelf)
+        {
+            pKeyTextToggle.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && pKeyTextToggle.activeSelf)
+        {
+            pKeyTextToggle.SetActive(false);
+        }
+    }
+
 }
